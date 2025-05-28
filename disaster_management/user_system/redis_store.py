768f320ecf1c59472ad5817e330f24a71f674cf3
Brij1998810 <brijeshnf2212@gmail.com>
2025-05-28 16:27:12 +0530

    Add Redis User Storage

---
+++ disaster_management/user_system/redis_store.py
@@ -0,0 +1,31 @@
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Load environment variables from .env file
+redis_host = os.getenv('REDIS_HOST')
+
+class RedisUserData:
+
+    def __init__(self, host=redis_host):
+        self.redis_client = redis.Redis(host=host, port=6379, db=0)
+
+    def get_user_data(self, user_id):
+        # Get user data from Redis
+        user_data = self.redis_client.get(user_id)
+
+    def set_user_data(self, user_id, data):
+        # Set user data in Redis
+        self.redis_client.set(user_id, data)
+
+    def delete_user_data(self, user_id):
+        # Delete user data from Redis
+        self.redis_client.delete(user_id)
+
+    def get_all_user_data(self):
+        # Get all user data from Redis
+        all_user_data = self.redis_client.keys('*')
+
+
