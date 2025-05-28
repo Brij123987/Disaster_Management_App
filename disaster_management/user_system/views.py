13869eb6b5844db6284ed214916816d26ec3bf5c
Brij1998810 <brijeshnf2212@gmail.com>
2025-05-28 17:11:55 +0530

    add serializers and logger

--- disaster_management/user_system/views.py
+++ disaster_management/user_system/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import json
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_user_data(request, user_id):
+    try:
+        pass
+    except Exception as e:
+        pass
