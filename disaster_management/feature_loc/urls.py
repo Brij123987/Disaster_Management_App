3354d95bdd00e83f66b983e1d6d86efe57fcedda
Brij1998810 <brijeshnf2212@gmail.com>
2025-06-02 09:56:30 +0530

    added api to get the earthquake historical data

--- disaster_management/feature_loc/urls.py
+++ disaster_management/feature_loc/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from feature_loc import views
 
 app_name = 'feature_loc'
 
 urlpatterns = [
-
+    path('get_location_earthquake_historical_data/', views.get_location_earthquake_historical_data, name='get_location_earthquake_historical_data'),
 ]
--- disaster_management/feature_loc/views.py
+++ disaster_management/feature_loc/views.py
@@ -1,3 +1,77 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from feature_loc.helpers.get_user_locations import get_location_coordinates
+import requests
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+EARTHQUAKE_HISTORICAL_DATA = os.getenv('EARTHQUAKE_HISTORICAL_URLS')
+
+import logging
+import logging.config
+from django.conf import settings
+
+# Apply Django's logging config
+logging.config.dictConfig(settings.LOGGING)
+logger = logging.getLogger('custom_logger')
+
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_location_earthquake_historical_data(request):
+    try:
+        location = request.query_params.get('location')
+
+        if not location:
+            return Response({'error': 'Location is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        urls = EARTHQUAKE_HISTORICAL_DATA
+
+        lat, lon = get_location_coordinates(location)
+
+        if not lat or not lon:
+            return Response({'error': 'Unable to get location'}, status=status.HTTP_400_BAD_REQUEST)
+
+        params = {
+            'format': 'geojson',
+            'latitude': lat,
+            'longitude': lon,
+            'maxradiuskm': 1000,
+            'starttime': '2016-01-01',
+            'endtime': '2016-01-02',
+        }
+
+        response = requests.get(urls, params=params)
+
+        if response.status_code != 200:
+            return Response({'error': 'Unable to get data'}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = response.json()
+
+        if not data or not data['features']:
+            return Response({'error': 'No data found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        for feature in data['features'][:5]:
+            props = feature['properties']
+            coords = feature['geometry']['coordinates']
+         
+        response_data = {
+            'location': location,
+            'latitude': lat,
+            'longitude': lon,
+            'Magnitude' : props['mag'],
+            'coords': coords
+        }
+
+        return Response({'data':response_data}, status=status.HTTP_200_OK)
+
+
+    except Exception as e:
+        logger.error(f"Error in get_location_earthquake_historical_data: {str(e)}")
+        return Response({"Error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
