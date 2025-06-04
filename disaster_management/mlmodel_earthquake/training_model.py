6f3ce8da0a96490b2a6909f42d10009d3d3f5f12
Brij1998810 <brijeshnf2212@gmail.com>
2025-06-04 11:11:16 +0530

    added logic for the predicted earthquake

--- disaster_management/mlmodel_earthquake/training_model.py
+++ disaster_management/mlmodel_earthquake/training_model.py
@@ -1,8 +1,13 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.pipeline import Pipeline
 from .ml_processing import model_processing
+import pandas as pd
 import joblib
 import numpy as np
 import os
+
+from mlmodel_earthquake.helpers.get_boundary_plate import updated_csv
 
 import logging
 import logging.config
@@ -19,6 +24,8 @@ BASE_DIR = os.getenv('BASE_DIR')
 
 MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
 SCALER_PATH = os.path.join(BASE_DIR, "scaler.pkl")
+
+PREDICT_MODEL_PATH = os.path.join(BASE_DIR, "predict_model.pkl")
 
 
 def train_save_model():
@@ -48,7 +55,10 @@ def train_save_model():
 
 def load_model(input_data):
     try:
-        train_save_model()
+        res = train_save_model()
+        if not res:
+            logger.error("Model is not trained")
+            return None
 
         model: RandomForestClassifier = joblib.load(MODEL_PATH)
         scaler = joblib.load(SCALER_PATH)
@@ -73,3 +83,50 @@ def load_model(input_data):
         logger.error(f"Error loading model: {str(e)}")
         return None
 
+
+def train_model_predict_next_eartquake():
+    try:
+        # Load data from DataFrame
+        df = updated_csv()
+
+        X = df[['Magnitude', 'Depth', 'TimeSeriesLast', 'PlateDistance']]
+        y = df['MagnitudeShifted']
+
+        # Pipeline with scaling and regression
+        model = Pipeline([
+            ('scaler', StandardScaler()),
+            ('regressor', RandomForestRegressor())
+        ])
+
+        model.fit(X, y)
+
+        # Save Predicted Model
+        joblib.dump(model, PREDICT_MODEL_PATH)
+
+        return True
+        
+    except Exception as e:
+        logger.error(f"train_model_predict_next_eartquake: {str(e)}")
+        return None
+    
+
+def train_model_predict_next_eartquake_with_custom_model(input_data):
+    try:
+        res = train_model_predict_next_eartquake()
+
+        if not res:
+            logger.error("Predicted Model is not available")
+            return None
+        
+        model: RandomForestRegressor = joblib.load(PREDICT_MODEL_PATH)
+
+        input_data = [[5.2, 60.0, 24.5, 10.0]]  # Magnitude, Depth, HoursSinceLast, PlateDistance (km)
+        predicted_magnitude = model.predict(input_data)
+
+        print(f"--------------100: {predicted_magnitude[0]:.1f}")
+
+        return f"{predicted_magnitude[0]:.1f}"
+    
+    except Exception as e:
+        logger.error(f"train_model_predict_next_eartquake_with_custom_model: {str(e)}")
+        return None
