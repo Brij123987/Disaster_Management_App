07c795fbb6e218c94cbc6ec01077aa59d064eb9f
Brij1998810 <brijeshnf2212@gmail.com>
2025-06-11 10:08:43 +0530

    trained earthquake models

--- disaster_management/feature_loc/views.py
+++ disaster_management/feature_loc/views.py
@@ -59,7 +59,7 @@ def get_location_earthquake_historical_data(request):
             'longitude': lon,
             'maxradiuskm': 1000,
             'starttime': '2025-05-01',
-            'endtime': '2025-06-09',
+            'endtime': '2025-06-11',
         }
 
         response = requests.get(urls, params=params)
