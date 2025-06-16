50b57a20485b0849003be3a37676b1a9d1224241
Brij1998810 <brijeshnf2212@gmail.com>
2025-06-16 11:24:08 +0530

    create api to get data in json fromat through the csv file

--- disaster_management/feature_loc/urls.py
+++ disaster_management/feature_loc/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get_location_earthquake_historical_data/', views.get_location_earthquake_historical_data, name='get_location_earthquake_historical_data'),
     path('get_cyclone_data/', views.get_cyclone_data, name="get_cyclone_data"),
     path('get_cyclone_prediction/', views.get_cyclone_prediction, name="get_cyclone_prediction"),
+    path('get_earthquake_data_json/', views.get_earthquake_data_json, name="get_earthquake_data_json"),
 ]
--- disaster_management/feature_loc/views.py
+++ disaster_management/feature_loc/views.py
@@ -22,6 +22,10 @@ from feature_loc.helpers.cyclone_data_sourcing_helpers import get_cyclone_histor
 from feature_loc.helpers.generate_start_end_date import get_start_date
 from cyclone_ml.predict_cyclone import train_cyclone_model
 
+from feature_loc.helpers.read_csv_data_helpers import get_eathquake_data_from_csv
+
+import pandas as pd
+
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -29,6 +33,8 @@ load_dotenv()
 EARTHQUAKE_HISTORICAL_DATA = os.getenv('EARTHQUAKE_HISTORICAL_URLS')
 
 CYCLONE_LOCATION_DATA = os.getenv('CYCLONE_LOCATION_DATA')
+
+BASE_DIR = os.getenv('BASE_DIR')
 
 import logging
 import logging.config
@@ -214,4 +220,22 @@ def get_cyclone_prediction(request):
 
     except Exception as e:
         logger.error(f"Error in get_cyclone_prediction: {str(e)}", exc_info= True)
+        return Response({"error message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET'])
+def get_earthquake_data_json(request):
+    try:
+        location = request.query_params.get('location')
+
+        earthquake_data = get_eathquake_data_from_csv(location)
+
+        if not earthquake_data:
+            return Response({'error': 'Unable to get earthquake data'}, status=status.HTTP_400_BAD_REQUEST)
+
+        
+        return Response({"data":earthquake_data}, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        logger.error(f"Error in get_earthquake_data_json: {str(e)}", exc_info=True)
         return Response({"error message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
