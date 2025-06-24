50ec095da30528d178db9a9e551ed7c0fcc5bfd4
Brijesh <brijeshyadav9811@gmail.com>
2025-06-25 00:24:24 +0530

    added is tracked field

--- disaster_management/user_system/models.py
+++ disaster_management/user_system/models.py
@@ -22,6 +22,7 @@ class UserLocationDetail(models.Model):
     longitude = models.CharField(max_length=200, blank=True, null=True)
     phonenumber = models.CharField(max_length=200, blank=True, null=True)
     country_code = models.CharField(max_length=10)
+    isTracked = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.email}, {self.location}"
