b9c7b02a4ffcc74943a4269759f909defb98547f
Brijesh <brijeshyadav9811@gmail.com>
2025-06-24 22:17:18 +0530

    added new model

--- disaster_management/user_system/models.py
+++ disaster_management/user_system/models.py
@@ -12,3 +12,21 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = 'User Table'
         db_table = 'user_table'
+
+class UserLocationDetail(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_location_detail')
+    location = models.CharField(max_length=200, blank=True, null=True)
+    latitude = models.CharField(max_length=200, blank=True, null=True)
+    longitude = models.CharField(max_length=200, blank=True, null=True)
+    phonenumber = models.CharField(max_length=200, blank=True, null=True)
+    country_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user.email}, {self.location}"
+    
+    class Meta:
+        verbose_name = 'User Location Detail Table'
+        db_table = 'user_location_detail_table'
+    
