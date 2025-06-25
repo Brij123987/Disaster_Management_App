e7ea7937c02ac2385a329468a37653efbf7517ee
Brij1998810 <brijeshnf2212@gmail.com>
2025-06-25 11:32:05 +0530

    added get_update_location

--- disaster_management/user_system/urls.py
+++ disaster_management/user_system/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from user_system import views
 from user_system.registration import user_register
+from user_system.track_location import trackingLocation
 
 app_name = "user_system"
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('createUser/', views.create_user_data, name='create_user_data'),
     path('register/', user_register.create_user, name='register'),
     path('login/',user_register.user_login, name='user_login'),
+    path('track-location/',trackingLocation.get_update_location, name='track_location'),
 ]
