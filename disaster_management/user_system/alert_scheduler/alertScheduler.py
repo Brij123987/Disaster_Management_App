cdbc38d98566c7b5b40a57e4ea5cae16c359fbfe
Brij1998810 <brijeshnf2212@gmail.com>
2025-06-26 12:00:59 +0530

    added scheduler for sending sms

---
+++ disaster_management/user_system/alert_scheduler/alertScheduler.py
@@ -0,0 +1,45 @@
+import requests
+from datetime import datetime
+
+def check_and_send_alerts():
+    from user_system.models import UserLocationDetail
+    from user_system.helpers.mobileNumberValidationHelper import send_alert_msg_twilio, send_alert_msg_vonage
+
+    print("Starting...............................")
+    API_URL = "http://127.0.0.1:8000"
+    date = datetime.today().strftime('%Y-%m-%d')
+
+    try:
+        userLocation = UserLocationDetail.objects.filter(isTracked=1).values('location', 'country_code', 'phonenumber')
+
+        for user in userLocation:
+            location = user['location']
+            phonenumber = user['country_code'] + user['phonenumber']
+
+            earthquake_response = requests.get(
+                f'{API_URL}/feature/get_location_earthquake_historical_data/?location={location}&date={date}'
+            )
+
+            if earthquake_response.status_code == 200:
+                data = earthquake_response.json()
+                if data.get('data', {}).get('predicted_data') == "High":
+                    magnitude = data.get('data', 0).get('Magnitude')
+                    send_alert_msg_vonage(phonenumber, "earthquake", magnitude, windSpeed=None, location=location)
+
+            cyclone_response = requests.get(
+                f'{API_URL}/feature/get_cyclone_prediction/?location={location}&end_date={date}'
+            )
+
+            if cyclone_response.status_code == 200:
+                data = cyclone_response.json()
+                if data.get('data', {}).get('CyclonePrediction') == "No cyclone is expected in this region.":
+                    print("Cyclone Data Fetch ")
+                    send_alert_msg_vonage(phonenumber, "cyclone", windSpeed=None, magnitude=None, location=location)
+        
+        print("Ended---------------------------------------------")
+
+    except Exception as e:
+        print(f"An error occurred during alert scheduling: {e}")
+
+
+
--- disaster_management/user_system/apps.py
+++ disaster_management/user_system/apps.py
@@ -1,6 +1,21 @@
+import os
 from django.apps import AppConfig
+from apscheduler.schedulers.background import BackgroundScheduler
+from user_system.alert_scheduler.alertScheduler import check_and_send_alerts
 
+scheduler = BackgroundScheduler()
 
 class UserSystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_system'
+
+    # def ready(self):
+    #     # Prevent the scheduler from running twice in development
+    #     if os.environ.get('RUN_MAIN') == 'true':
+    #         self.schedule_job()
+
+    # def schedule_job(self):
+    #     # Check if job is already scheduled (optional if using global scheduler)
+    #     if not scheduler.get_jobs():
+    #         scheduler.add_job(check_and_send_alerts, 'interval', minutes=1)
+    #         scheduler.start()
